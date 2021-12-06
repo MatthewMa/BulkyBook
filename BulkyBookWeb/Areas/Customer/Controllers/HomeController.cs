7f896627373a81dec7ef166536dfe06e41018ffc
MatthewMa <masihua721@gmail.com>
2021-12-05 20:48:25 -0700

    Home page component implement

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using BulkyBook.Models;
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,20 +14,29 @@ namespace BulkyBookWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWOrk;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWOrk)
         {
             _logger = logger;
+            _unitOfWOrk = unitOfWOrk;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWOrk.Product.GetAll(includeProperties: "Category,CoverType");
+
+            return View(productList);
         }
 
-        public IActionResult Privacy()
+        public IActionResult Details(int id)
         {
-            return View();
+            ShoppingCart cart = new()
+            {
+                Product = _unitOfWOrk.Product.GetFirstOrDefault(p => p.Id == id, includeProperties: "Category,CoverType"),
+                Count = 1
+            };
+            return View(cart);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
